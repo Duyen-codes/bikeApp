b8f32ba58fcdd0d6d1bd21cb6f8dd49c1f94987a
Duyen-codes <hongduyen0705@gmail.com>
2023-01-10 19:01:30 +0200

    built homepage UI

--- bikeApp-frontend/src/components/Home.js
+++ bikeApp-frontend/src/components/Home.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
 
 const Home = () => {
 	return (
 		<div>
-			<h1>bikeApp</h1>
-			homepage
+			<Typography variant='h2' component='h2' align='center' my={3}>
+				City bikes
+			</Typography>
+			<Container maxWidth='sm'>
+				<Typography>
+					This application shows data of city bikes in the Helsinki Capital
+					area.
+				</Typography>
+
+				<Typography>
+					Explore stations {""}
+					<Link
+						href='/stations'
+						sx={{ color: "#2196f3", display: "inline", textDecoration: "none" }}
+					>
+						here
+					</Link>{" "}
+				</Typography>
+				<Typography>
+					Explore journeys{" "}
+					<Link
+						href='/journeys'
+						sx={{ color: "#2196f3", display: "inline", textDecoration: "none" }}
+					>
+						here
+					</Link>{" "}
+				</Typography>
+			</Container>
 		</div>
 	);
 };
