03b72c03a844716b5cbf5601458a35c87737a65c
Duyen-codes <hongduyen0705@gmail.com>
2023-01-13 09:42:42 +0200

    built stationDetails ui

--- bikeApp-frontend/src/components/StationDetails.js
+++ bikeApp-frontend/src/components/StationDetails.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect, useRef, Children } from "react";
 import axios from "axios";
 import { useLocation, useParams } from "react-router-dom";
-import { Container, Stack, Typography } from "@mui/material";
+import {
+	Card,
+	CardContent,
+	Container,
+	List,
+	Paper,
+	Stack,
+	Typography,
+} from "@mui/material";
+import ListItem from "@mui/material/ListItem";
 import Box from "@mui/material/Box";
+
+import Grid from "@mui/material/Grid";
 import GoogleMap from "./GoogleMap";
 import { Fade } from "react-awesome-reveal";
 import CircularProgress from "@mui/material/CircularProgress";
+import { borderRight } from "@mui/system";
 
 const StationDetails = (props) => {
 	const { state } = useLocation();
@@ -73,8 +85,9 @@ const StationDetails = (props) => {
 		<Fade>
 			<Container
 				sx={{
-					paddingTop: "7rem",
+					paddingTop: "5rem",
 					textAlign: "center",
+					marginBottom: "5rem",
 				}}
 			>
 				<Typography variant='h2' fontWeight='bold'>
@@ -84,43 +97,64 @@ const StationDetails = (props) => {
 					Address: {station.Adress}, {station.Kaupunki}
 				</Typography>
 
-				<p>
-					Total number of journeys starting from the station:{" "}
-					{departuresFromStationCount}
-				</p>
-				<p>
-					Total number of journeys ending at the station:{" "}
-					{returnsAtStationCount}
-				</p>
-				<p>
-					The average distance of a journey starting from the station:
-					{departureAvgDistance}m
-				</p>
-				<p>
-					The average distance of a journey ending at the station:
-					{returnAvgDistance}m
-				</p>
-				<div>
-					Top 5 most popular return stations for journeys starting from the
-					station:
-					{top5ReturnStations.map((station) => (
-						<li key={station._id}>
-							{station._id}
-							<p>Returns: {station.count}</p>
-						</li>
-					))}
-				</div>
+				<Grid
+					container
+					spacing={{ xs: 2, md: 3 }}
+					sx={{ borderColor: "#e0e0e0", pt: "3rem", mb: "3rem" }}
+				>
+					<Grid item xs={12} sm={6} md={4} sx={{ borderColor: "#e0e0e0" }}>
+						<Card sx={{ height: "100%" }}>
+							<CardContent>
+								<p>
+									Total number of journeys starting from the station:{" "}
+									{departuresFromStationCount}
+								</p>
+								<p>
+									Total number of journeys ending at the station:{" "}
+									{returnsAtStationCount}
+								</p>
+								<p>
+									The average distance of a journey starting from the station:
+									{departureAvgDistance}m
+								</p>
+								<p>
+									The average distance of a journey ending at the station:
+									{returnAvgDistance}m
+								</p>
+							</CardContent>
+						</Card>
+					</Grid>
 
-				<div>
-					Top 5 most popular departure stations for journeys returning at the
-					station:
-					{top5DepartureStations.map((station) => (
-						<li key={station._id}>
-							{station._id}
-							<p>Departures: {station.count}</p>
-						</li>
-					))}
-				</div>
+					<Grid item xs={12} sm={6} md={4}>
+						<Card>
+							<CardContent>
+								Top 5 most popular return stations for journeys starting from
+								the station:
+								{top5ReturnStations.map((station) => (
+									<li key={station._id}>
+										{station._id}
+										<p>Returns: {station.count}</p>
+									</li>
+								))}
+							</CardContent>
+						</Card>
+					</Grid>
+
+					<Grid item xs={12} sm={6} md={4}>
+						<Card sx={{ height: "100%" }}>
+							<CardContent>
+								Top 5 most popular departure stations for journeys returning at
+								the station:
+								{top5DepartureStations.map((station) => (
+									<li key={station._id}>
+										{station._id}
+										<p>Departures: {station.count}</p>
+									</li>
+								))}
+							</CardContent>
+						</Card>
+					</Grid>
+				</Grid>
 
 				<GoogleMap station={station} />
 			</Container>
