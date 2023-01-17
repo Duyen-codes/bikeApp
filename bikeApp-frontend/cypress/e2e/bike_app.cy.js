b76198635b45eb2e9a97331be9b00e5e4f5d6c5a
Duyen-codes <hongduyen0705@gmail.com>
2023-01-17 14:23:35 +0200

    write tests for opening pages from navbar

--- bikeApp-frontend/cypress/e2e/bike_app.cy.js
+++ bikeApp-frontend/cypress/e2e/bike_app.cy.js
@@ -1,7 +1,18 @@
 describe("Citybike app", function () {
-	it("front page can be opened", function () {
+	beforeEach(function () {
 		cy.visit("http://localhost:3000/");
+	});
+
+	it("front page can be opened", function () {
 		cy.contains("Helsinki city bikes");
 		cy.contains("stations");
+	});
+
+	it("stations page can be opened", function () {
+		cy.contains("stations").click();
+	});
+
+	it("journeys page can be opened", function () {
+		cy.contains("journeys").click();
 	});
 });
