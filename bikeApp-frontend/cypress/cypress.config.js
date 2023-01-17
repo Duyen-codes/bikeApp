af7d17f7ef8755181929e3a3e4e16d3eee174407
Duyen-codes <hongduyen0705@gmail.com>
2023-01-17 14:59:54 +0200

    e2e test resetup and initial tests

---
+++ bikeApp-frontend/cypress/cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+	e2e: {
+		supportFile: false,
+		specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+});
--- bikeApp-frontend/cypress/e2e/city_bike.cy.js
+++ bikeApp-frontend/cypress/e2e/city_bike.cy.js
@@ -1,18 +1,14 @@
 describe("Citybike app", function () {
 	beforeEach(function () {
-		cy.visit("http://localhost:3000/");
+		cy.visit("http://localhost:3000");
 	});
 
 	it("front page can be opened", function () {
 		cy.contains("Helsinki city bikes");
-		cy.contains("stations");
 	});
 
 	it("stations page can be opened", function () {
 		cy.contains("stations").click();
-	});
-
-	it("journeys page can be opened", function () {
-		cy.contains("journeys").click();
+		cy.contains("Stations");
 	});
 });
