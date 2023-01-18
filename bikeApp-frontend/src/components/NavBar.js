42cc4d14e5e0bc5aae5e7e7199f3dd34178481b8
Duyen-codes <hongduyen0705@gmail.com>
2023-01-18 11:48:40 +0200

    remove unused imports in NavBar

--- bikeApp-frontend/src/components/NavBar.js
+++ bikeApp-frontend/src/components/NavBar.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,13 +8,9 @@ import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
-import { NavLink } from "react-router-dom";
 import Link from "@mui/material/Link";
 
 const drawerWidth = 240;
