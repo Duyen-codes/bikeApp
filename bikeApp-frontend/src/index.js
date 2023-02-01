d74674713807e17b13ea9bf8a2e31e9fad358e36
Duyen-codes <hongduyen0705@gmail.com>
2023-02-01 17:44:40 +0200

    remove extra imports

--- bikeApp-frontend/src/index.js
+++ bikeApp-frontend/src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useMatch,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Stations from "./components/Stations";
 import Home from "./components/Home";
 import Journeys from "./components/Journeys";
